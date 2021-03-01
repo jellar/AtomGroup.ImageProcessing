efe11d3f5c2e96e4f3ca157d0caf63038bd89eac
Rajesh Jella <jella.rajesh@gmail.com>
2021-03-01 20:49:57 +0000

    Initial commit.

---
+++ ImageProcessing.Core/ImageResolution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+
+namespace ImageProcessing.Core
+{
+    public static class ImageResolution
+    {
+        public static Image SetResizeResolution(this Image img, int width, int xDpi, int yDpi)
+        {
+            return null;
+        }
+    }
+    
+    
+}
